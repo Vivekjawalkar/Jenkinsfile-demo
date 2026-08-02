14b4edaa23d4a0bb28b27f60e183fd2f27e0b3f4
Vivek <vjawalkar99@gmail.com>
2026-08-02 16:26:43 +0000

    maven file added

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,9 @@
+public class HelloApp {
+ 
+   public static void main(String[] args) {
+ 
+      System.out.println("Hello from Maven!");
+      System.out.println("Welcome to Jenkins CI/CD");
+ 
+   }
+}
