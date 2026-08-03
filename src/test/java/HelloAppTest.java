8a23d2f9a6392e0d8279699550a1004bb72c7528
Vivek <vjawalkar99@gmail.com>
2026-08-03 05:35:29 +0000

    HelloAppTest.java added

---
+++ src/test/java/HelloAppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+ 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+ 
+public class HelloAppTest {
+ 
+        @Test
+        void testAddition() {
+                assertEquals(5, 2 + 3);
+        }
+}
